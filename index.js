263ef585bfa1b32730790a687f0adda413e4278a
Ron <ronmwangi00@gmail.com>
2023-06-10 13:21:22 +0300

    Added JavaScript file

---
+++ index.js
@@ -0,0 +1,15 @@
+function bigText(x){
+    x.style.fontSize = "30px";
+
+}
+function normalText(x){
+    x.style.fontSize = "20px";
+}
+function enlarge(x){
+    x.style.height = "150";
+    x.style.width = "150";
+}
+function normal(x){
+    x.style.height = "128";
+    x.style.width = "128";
+}
