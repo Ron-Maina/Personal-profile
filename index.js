262ac74ed96f8c1f365555e6a2537cdac8150914
Ron <ronmwangi00@gmail.com>
2023-06-10 13:26:43 +0300

    Javascript changes

--- index.js
+++ index.js
@@ -5,11 +5,3 @@ function bigText(x){
 function normalText(x){
     x.style.fontSize = "20px";
 }
-function enlarge(x){
-    x.style.height = "150";
-    x.style.width = "150";
-}
-function normal(x){
-    x.style.height = "128";
-    x.style.width = "128";
-}
