4e82967e4e1711c7042c1dbb1519910ac1e05a61
Ron <ronmwangi00@gmail.com>
2023-06-12 03:30:19 +0300

    Final Project

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function bigText(x){
 function normalText(x){
     x.style.fontSize = "20px";
 }
+function hide(x) {
+    x.getElementById('imageText1').hidden = true
+ }
+ function show(x) {
+    x.getElementById('imageText1').hidden = false
+ }
+ function hide(x) {
+    x.getElementById('imageText2').hidden = true
+ }
+ function show(x) {
+    x.getElementById('imageText2').hidden = false
+ }
